843a7e4fb3d509c75fcb16fa5dd58780ff065399
Tobias <1056318+tobias-gaenzler@users.noreply.github.com>
2021-01-06 22:56:06 +0100

    Removed synchronization by using concurrent sets and performing check immediately.

--- src/main/java/de/tobiasgaenzler/pegsolitaire/solver/strategy/ParallelStreamStrategy.java
+++ src/main/java/de/tobiasgaenzler/pegsolitaire/solver/strategy/ParallelStreamStrategy.java
@@ -21,19 +21,15 @@ import java.util.stream.StreamSupport;
  */
 @Component
 public class ParallelStreamStrategy implements WinningPositionsStrategy {
+
     private static final Logger logger = LoggerFactory.getLogger(ParallelStreamStrategy.class);
-
-
-    private final List<Set<Long>> reachablePositions = new ArrayList<>();
+    private final List<Set<Long>> reachablePositions = Collections.synchronizedList(new ArrayList<>());
 
     @Override
     public List<Set<Long>> solve(Board board, Long startPosition) {
         reachablePositions.clear();
         assembleReachablePositions(board, startPosition);
         long start = System.currentTimeMillis();
-        check(board);
-        logger.info("Time check: {}", (System.currentTimeMillis() - start));
-        start = System.currentTimeMillis();
         removeNonWinningPositions(board);
         long totalSolutionTime = System.currentTimeMillis() - start;
         logger.info("Time non winning positions: {}\n", totalSolutionTime);
@@ -64,34 +60,85 @@ public class ParallelStreamStrategy implements WinningPositionsStrategy {
         }
     }
 
+
     /**
-     * Due to concurrency there might be symmetric entries in the sets of reachable positions.
-     * Remove symmetric entries from reachable positions.
+     * Assemble all reachable positions starting with startPosition.
+     * Use the stream().parallel() for concurrency.
      *
-     * @param board Board where the positions live
+     * @param board         the board where the positions live
+     * @param startPosition start finding positions with this start position
      */
-    private void check(Board board) {
-        for (int pins = 0; pins < reachablePositions.size(); pins++) {
-            Set<Long> redundantPositions = ConcurrentHashMap.newKeySet();
+    private void assembleReachablePositions(Board board, Long startPosition) {
+        int numberOfStartPins = board.getNumberOfPegs(startPosition);
+
+        for (int i = 0; i <= numberOfStartPins + 1; i++) {
+            reachablePositions.add(ConcurrentHashMap.newKeySet());
+        }
+
+        // add startPosition to reachablePositions
+        reachablePositions.get(numberOfStartPins).add(startPosition);
+
+        long totalTime = 0L;
+        for (int numberOfRemainingPieces = numberOfStartPins - 1; (numberOfRemainingPieces > 0); numberOfRemainingPieces--) {
             long start = System.currentTimeMillis();
-            Set<Long> positions = reachablePositions.get(pins);
-            positions.stream().parallel().forEach(position -> {
-                for (long symmetricPosition : board.getSymmetricPositions(position)) {
-                    if (symmetricPosition != position && positions.contains(symmetricPosition)) {
-                        redundantPositions.add(position);
-                        break;
+            Set<Long> followingPositions = reachablePositions.get(numberOfRemainingPieces);
+            reachablePositions.get(numberOfRemainingPieces + 1).stream().parallel().forEach(currentPosition -> {
+                for (long consecutivePosition : board.getConsecutivePositions(currentPosition)) {
+                    if (followingPositions.contains(consecutivePosition)) {
+                        continue;
+                    }
+                    boolean inSet = false;
+                    for (long symPos : board.getSymmetricPositions(consecutivePosition)) {
+                        //check if a symmetric position is already in the set
+                        if (followingPositions.contains(symPos)) {
+                            inSet = true;
+                            break;
+                        }
+                    }
+                    //add position if no equivalent (symmetric) position is already in set
+                    if (!inSet) {
+                        followingPositions.add(consecutivePosition);
                     }
                 }
             });
-            for (Long redundantPosition : redundantPositions) {
-                for (long symmetricPosition : board.getSymmetricPositions(redundantPosition)) {
-                    if (symmetricPosition != redundantPosition && positions.contains(symmetricPosition)) {
-                        positions.remove(redundantPosition);
-                        break;
-                    }
+            totalTime += (System.currentTimeMillis() - start);
+            logger.info("{}: {} reachable positions in {} ms", numberOfRemainingPieces, reachablePositions.get(numberOfRemainingPieces).size(),
+                    (System.currentTimeMillis() - start));
+            removeSymmetricPositions(board, followingPositions);
+        }
+
+        logger.info("TOTAL TIME REACHABLE POSITIONS: {} ms", totalTime);
+    }
+
+    /**
+     * Due to concurrency there might be symmetric entries in the sets of reachable positions.
+     * Remove symmetric entries from reachable positions.
+     * Removing the positions after calculation is faster than preventing that symmetric positions are added
+     * (using synchronization).
+     *
+     * @param board Board where the positions live
+     */
+    private void removeSymmetricPositions(Board board, Set<Long> positions) {
+        Set<Long> redundantPositions = ConcurrentHashMap.newKeySet();
+        long start = System.currentTimeMillis();
+        positions.stream().parallel().forEach(position -> {
+            for (long symmetricPosition : board.getSymmetricPositions(position)) {
+                if (symmetricPosition != position && positions.contains(symmetricPosition)) {
+                    redundantPositions.add(position);
+                    break;
                 }
             }
-            logger.info("Check {}: {}, Time: {}", pins, redundantPositions.size(), (System.currentTimeMillis() - start));
+        });
+        for (Long redundantPosition : redundantPositions) {
+            for (long symmetricPosition : board.getSymmetricPositions(redundantPosition)) {
+                if (symmetricPosition != redundantPosition && positions.contains(symmetricPosition)) {
+                    positions.remove(redundantPosition);
+                    break;
+                }
+            }
+        }
+        if (!redundantPositions.isEmpty()) {
+            logger.info("Removed {} symmetric positions in {} ms", redundantPositions.size(), (System.currentTimeMillis() - start));
         }
     }
 
@@ -118,56 +165,5 @@ public class ParallelStreamStrategy implements WinningPositionsStrategy {
             logger.info("{}: {} winning positions (all: {})", pegs, winningPositions.size(), positions.size());
             reachablePositions.set(pegs, winningPositions);
         }
-    }
-
-    /**
-     * Assemble all reachable positions starting with startPosition.
-     * Use the stream().parallel() for concurrency.
-     *
-     * @param board         the board where the positions live
-     * @param startPosition start finding positions with this start position
-     */
-    private void assembleReachablePositions(Board board, Long startPosition) {
-        int numberOfStartPins = board.getNumberOfPegs(startPosition);
-
-        for (int i = 0; i <= numberOfStartPins + 1; i++) {
-            reachablePositions.add(new HashSet<>());
-        }
-
-        // add startPosition to reachablePositions
-        reachablePositions.get(numberOfStartPins).add(startPosition);
-
-        long totalTime = 0L;
-        for (int numberOfRemainingPieces = numberOfStartPins - 1; (numberOfRemainingPieces > 0); numberOfRemainingPieces--) {
-            long start = System.currentTimeMillis();
-            Set<Long> followingPositions = reachablePositions.get(numberOfRemainingPieces);
-            reachablePositions.get(numberOfRemainingPieces + 1).stream().parallel().forEach(currentPosition -> {
-                for (long consecutivePosition : board.getConsecutivePositions(currentPosition)) {
-                    // synchronizing on the _contains_ operations is too slow
-                    if (followingPositions.contains(consecutivePosition)) {
-                        continue;
-                    }
-                    boolean inSet = false;
-                    for (long symPos : board.getSymmetricPositions(consecutivePosition)) {
-                        //check if a symmetric position is already in the set
-                        if (followingPositions.contains(symPos)) {
-                            inSet = true;
-                            break;
-                        }
-                    }
-                    //add position if no equivalent (symmetric) position is already in set
-                    if (!inSet) {
-                        // synchronizing on inserts is a good idea
-                        synchronized (followingPositions) {
-                            followingPositions.add(consecutivePosition);
-                        }
-                    }
-                }
-            });
-            totalTime += (System.currentTimeMillis() - start);
-            logger.info("{}: {} positions in {} ms", numberOfRemainingPieces, reachablePositions.get(numberOfRemainingPieces).size(),
-                    (System.currentTimeMillis() - start));
-        }
-        logger.info("TOTAL TIME REACHABLE POSITIONS: {} ms", totalTime);
     }
 }
